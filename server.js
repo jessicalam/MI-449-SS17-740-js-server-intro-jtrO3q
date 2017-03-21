ea4f5e221d23f6bd0cb3b44e301cb1d5a60e2795
jessicalam <j.lam@mail.com>
2017-03-21 18:27:59 -0400

    used .write instead of .end

--- server.js
+++ server.js
@@ -1,45 +1,30 @@
 // Require Node's http module and assign it to a variable
 var http = require('http')
 
-// Create a new server that just is a whale at every route
+// Create a new server that just says "Hi!!" at every route
 var server = http.createServer(function (request, response) {
   if (request.url === '/') {
-    response.end(
-      '<h1>Hello</h1>'
-    )
-  } else if (request.url === '/randomjokenumber') {
-    randomJoke = Math.random()*4;
-    window.onload = function() {
-      response.end(
-    	if (randomJoke == 1) {
-    		<p> Why did the donut visit the dentist? </p>
-        <p> To get a new filling. </p>)
-    	}
-
-    	if (randomJoke == 2) {
-        response.end(
-        <p>Why did the bee marry? </p>
-        <p>He’s finally found his honey. </p>)
-      }
-
-    	if (randomJoke == 3) {
-        response.end(
-        <p>Why does it suck to be a penguin?  </p>
-        <p>Because even when you get angry, you still look cute.</p>
-        )
-    	}
-
-
-}
+    response.end('<h1>HELLO</h1>')
+  } else if (request.url === '/randomjoke') {
+    response.write('<h1>Get ready for an amazing joke!</h1>')
+    var randomJoke = Math.random() * 4
+    if (randomJoke === 1) {
+      response.write('<p>Why did the donut visit the dentist? To get a new filling.</p>')
+    } else if (randomJoke === 2) {
+      response.write('<p>Why did the bee marry? He’s finally found his honey.</p>')
+    } else if (randomJoke === 3) {
+      response.write('<p>Why does it suck to be a penguin? Because even when you get angry, you still look cute.</p>')
+    }
+    response.end('<p>Return to the homepage <a href="/">here</a>!</p>')
   } else if (request.url === '/cuteness') {
-    response.end('<img src="/cuteness.jpg" alt="Waving hi">')
+    response.write('<img src="cuteness.jpg" alt="cute animal">')
+    response.end('<p>Return to the homepage <a href="/">here</a>!</p>')
   } else {
-    <h1> ERROR 404 </h1>
+    response.write('<h1>ERROR 404</h1><h3>The requested URL ' + request.url + ' could not be found!</h3p>')
+    response.end('<p>Return to the homepage <a href="/">here</a>!</p>')
   }
 })
 
-// Listen on port 8080, so that we can reach the app at
-// localhost:8080
 var port = process.env.PORT || 8080
 server.listen(port)
 
