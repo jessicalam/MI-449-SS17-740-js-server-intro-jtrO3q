9f713cd12b2d59180728f4b793de0b6bdc19ef86
jessicalam <j.lam@mail.com>
2017-03-28 15:43:45 -0400

    changed the html

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ var server = http.createServer(function (request, response) {
   } else if (request.url === '/randomjoke') {
     // response.write('<h1>Get ready for an amazing joke!</h1>')
     var randomJoke = Math.random() * 4
-    if (randomJoke === 1) {
-      response.end('<p>Why did the donut visit the dentist? To get a new filling.</p>')
-    } else if (randomJoke === 2) {
-      response.end('<p>Why did the bee marry? He’s finally found his honey.</p>')
-    } else if (randomJoke === 3) {
-      response.end('<p>Why does it suck to be a penguin? Because even when you get angry, you still look cute.</p>')
+    if (randomJoke <= 1) {
+      response.write('<p>Why did the donut visit the dentist? To get a new filling.</p>')
+    } else if (randomJoke <= 2) {
+      response.write('<p>Why did the bee marry? He’s finally found his honey.</p>')
+    } else {
+      response.write('<p>Why does it suck to be a penguin? Because even when you get angry, you still look cute.</p>')
     }
     response.end('<p>Return to the homepage <a href="/">here</a>!</p>')
   } else if (request.url === '/cuteness') {
-    response.write('<img src="https://static.pexels.com/photos/8482/animal-dog-pet-indoors.jpg" alt="cute animal">')
+    response.write('<h1>cute animal hopefully</h1><img src="https://static.pexels.com/photos/8482/animal-dog-pet-indoors.jpg" alt="cute animal" height="450px" width="450px">')
     response.end('<p>Return to the homepage <a href="/">here</a>!</p>')
   } else {
     response.write('<h1>ERROR 404</h1><h3>The requested URL ' + request.url + ' could not be found!</h3></p>')
